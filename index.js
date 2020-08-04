a913b690ab3473aa84e06b0db89eb2a80a4873d5
Leon Arndt <neotalon27@gmail.com>
2020-08-04 10:29:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * This example is intended to show a basic plain vanilla setup and
+ * also to be run as integration test for concurrency issues.
+ *
+ * Please remove setTimeout(), if you intend to use it as a blueprint!
+ *
+ */
+
+// require modules
+let http = require('http'),
+    i18n = require('i18n'),
+    app;
+
+// minimal config
+i18n.configure({
+    locales: ['en', 'de'],
+    directory: __dirname + '/locales'
+});
+
+// simple server
+app = http.createServer(function (req, res) {
+
+    // init & guess
+    i18n.init(req, res);
+
+    console.log("Hello mister");
+});
+
+// startup
+app.listen(3000, '127.0.0.1');
